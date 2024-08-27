e1ab171db84677a3e29db66ef63606eaf79a2e3f
hushmoon <3220102320@gmail.com>
2024-08-28 01:31:02 +0800

    lab09

--- lab/lab09/lab09.scm
+++ lab/lab09/lab09.scm
@@ -1,6 +1,14 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+  (cond ((= num1 num2) 0)
+        ((> num1 num2) 1)
+        ((< num1 num2) -1)
+    )
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+  (lambda (inc)
+   (+ num inc))
+)
 
 (define (composed f g) 'YOUR-CODE-HERE)
 
